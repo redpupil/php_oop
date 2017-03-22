c782e3d75da24ba781f668a5eb9a79a86f5db964
redpupil <riazkhan.cse@gmail.com>
2017-03-23 00:38:58 +0100

    scope resolution operator-netbeans

---
+++ oop2/scope_resolution.php
@@ -0,0 +1,14 @@
+<?php include 'inc/header.php'; ?>
+<h2>Topics: Scope Resolution Operator</h2>
+<div class="content">
+    <div class="topics">
+
+
+    <?php
+
+        echo 'Hello PHP OOP!';
+
+    ?>
+    </div>
+</div>
+<?php include 'inc/footer.php'; ?>
