3f6211cce69bf94b742dfd870b1b529d89f22af4
redpupil <riazkhan.cse@gmail.com>
2017-04-03 21:47:32 +0200

    anonymous

--- oop2/anonymous_classes.php
+++ oop2/anonymous_classes.php
@@ -3,31 +3,59 @@
 <div class="content">
     <div class="topics">
 
+    <?php
+        //useful when simple, one-off objects need to be created.
+        class SomeClass {}
+        interface SomeInterface {}
+        trait SomeTrait {}
+
+        var_dump(new class(10) extends SomeClass implements SomeInterface {
+            private $num;
+
+            public function __construct($num)
+            {
+                $this->num = $num;
+            }
+
+            use SomeTrait;
+        });
+    ?>
+
 
     <?php
-
-        //useful when simple, one-off objects need to be created.
-
-    // Pre PHP 7 code
-    class Logger
-    {
-        public function log($msg)
+        //nesting
+        class Outer
         {
-            echo $msg;
+            private $prop = 1;
+            protected $prop2 = 2;
+
+            protected function func1()
+            {
+                return 3;
+            }
+
+            public function func2()
+            {
+                return new class(/*$this->prop error*/) extends Outer {
+                    private $prop3;
+
+                    public function __construct($prop)
+                    {
+                        $this->prop3 = $prop;
+                    }
+
+                    public function func3()
+                    {
+                        return $this->prop2 + $this->prop3 + $this->func1();
+                    }
+                };
+            }
         }
-    }
 
-    $util->setLogger(new Logger());
-
-    // PHP 7+ code
-    $util->setLogger(new class {
-        public function log($msg)
-        {
-            echo $msg;
-        }
-    });
-
+        echo (new Outer)->func2()->func3();
     ?>
+
+
     </div>
 </div>
 <?php include 'inc/footer.php'; ?>
