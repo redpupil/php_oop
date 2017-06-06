2c4a36d2cf0c25e8203526d889eb8b9faf58a42d
redpupil <riazkhan.cse@gmail.com>
2017-06-06 10:21:52 +0200

    mysql

---
+++ mysql/inc/footer.php
@@ -0,0 +1,9 @@
+ 
+</section>
+<section class="footeroption">
+        <img src="php.png">
+        <p>&COPY;<?php echo date('Y');?> All rights reserved by Khan Network</p>
+	</section>
+</div>
+</body>
+</html>
