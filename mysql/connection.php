68250ccfab6e2158b89b6eee8c97e79140b4b281
Md. Riazul Islam Khan <riazkhan.cse@gmail.com>
2017-11-07 17:37:19 +0100

    connection

---
+++ mysql/connection.php
@@ -0,0 +1,14 @@
+<?php include 'inc/header.php'; ?>
+<h2>Topics: OOP connection way</h2>
+<div class="content">
+    <div class="topics">
+
+
+    <?php
+
+        $connection = mysqli
+
+    ?>
+    </div>
+</div>
+<?php include 'inc/footer.php'; ?>
